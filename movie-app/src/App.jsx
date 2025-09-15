33a9b51d64c4fbdeb2ddb65694f04738d4b93188
Prakhyath <prakhyath1501@gmail.com>
2025-09-15 15:15:38 +0530

    Initial Template

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <img src="./hero.png" alt='Hero Banner' />
+          <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy Movies without the hassle</h1>
+        </header>
+      
+      </div>
+
+    </main>
+  )
+}
+
+export default App
